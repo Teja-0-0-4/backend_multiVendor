0b1efaed5bd5cb2f501a9a8b28ee89e6fe60815a
Kukatla Teja <tejaspix1@gmail.com>
2025-02-01 22:57:15 +0530

    made some extra changes

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
-const express = require("express")
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const vendorRoutes = require('./routes/vendorRoutes')
-const firmRoutes = require('./routes/firmRoutes')
-const productRoutes = require('./routes/productRoutes');
-const path = require('path');
-const cors=require('cors');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const vendorRoutes = require("./routes/vendorRoutes");
+const firmRoutes = require("./routes/firmRoutes");
+const productRoutes = require("./routes/productRoutes");
+const path = require("path");
+const cors = require("cors");
 
-const app = express()
-
-const PORT = process.env.PORT||4000;
-
+const app = express();
 dotenv.config();
-mongoose.connect(process.env.MONGO_URL)
-    .then(()=>console.log("Mongodb connected successfully"))
-    .catch((error)=>console.log(error))
 
-app.use(cors)    
-app.use(bodyParser.json())
-app.use('/vendor',vendorRoutes)
-app.use('/firm',firmRoutes)
-app.use('/product',productRoutes)
-app.use('/uploads',express.static('uploads'));
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT,()=>{
-    console.log(`server started and running at ${PORT}`)
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  });
 
-app.use('/',(req,res)=>{
-     res.send("<h1> Welcome to home ");
-})
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/vendor", vendorRoutes);
+app.use("/firm", firmRoutes);
+app.use("/product", productRoutes);
+app.use("/uploads", express.static("uploads"));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to home</h1>");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server started and running at http://0.0.0.0:${PORT}`);
+});
